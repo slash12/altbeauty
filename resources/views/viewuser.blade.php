9a4611d6c9a0796bd5cfe758910f717096e8c93a
mgardasaib@umail.utm.ac.mu <ybuchiah.stg@ASTEKMTIUS>
2018-06-15 09:11:07 +0400

    template was added and integrated 150618 0911

---
+++ resources/views/viewuser.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">View Users</h1>
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+    
+
+
+@endsection
