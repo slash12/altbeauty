78efeccd1969fe780ce6de41078ff1ddd1ba6a96
Bibi Falak Nooree Khayrattee <nkhayrattee@astek.mu>
2018-06-18 14:20:06 +0400

    changing ALT to ALTs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ALT
+                    ALTs
                 </div>
 
                 <div class="links">
