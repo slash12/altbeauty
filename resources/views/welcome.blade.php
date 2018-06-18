46468cc91d59b5256398568d0575f98886ffa52e
Abdallah Peerally <apeerally.stg@astek.mu>
2018-06-18 14:42:50 +0400

    renaming altsss to altsssszzz

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ALTsssss
+                    ALTssssszzzz
                 </div>
 
                 <div class="links">
