bafcac7176162f01b7063a432f3e0b05c334f248
Hiteishee Hanoomanjee <hhanoomanjee.stg@astek.mu>
2018-06-18 14:33:56 +0400

    changing name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ALTs
+                    Laravel
                 </div>
 
                 <div class="links">
