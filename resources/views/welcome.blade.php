011e484c8f242fd34a3d8daab1163797ccc8f0c1
Bibi Falak Nooree Khayrattee <nkhayrattee@astek.mu>
2018-06-18 14:34:06 +0400

    changing ALTs to ALTsssss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ALTs
+                    ALTsssss
                 </div>
 
                 <div class="links">
