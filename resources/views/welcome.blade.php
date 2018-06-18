a54083ab75f0905dc92db6507988f1fb7728856d
nad12 <ngardasahib.stg@astek.mu>
2018-06-18 14:32:52 +0400

    change alts to altssssss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ALTs
+                    ALTsssssssssssssss
                 </div>
 
                 <div class="links">
