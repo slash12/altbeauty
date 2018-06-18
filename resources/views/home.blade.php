85f700b86b00e8b6649d2ad49469abe6d20a532c
Bibi Falak Nooree Khayrattee <nkhayrattee@astek.mu>
2018-06-18 14:12:21 +0400

    Searching for conflicts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged out
                 </div>
             </div>
         </div>
